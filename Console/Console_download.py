2b3496878a5e18d55138ba212a456d2186cc86f4
Johan <grimmerw968@gmail.com>
2020-11-13 17:04:38 +0200

    Youtube dowload, not the cleanest code

--- Console/Console_download.py
+++ Console/Console_download.py
@@ -1,77 +1,99 @@
+#Check in this script is awfull
+
 import re
 from pytube import YouTube
 from urllib.parse import urlparse
 
 
-download_link = input("Input links to download: ")
+# CONST and PATTERN
+running = True
+template_video = " type=video"
+template_music = " type=music"
+
+
+download_link = input("Input link to download: ")
 yt = YouTube(download_link)
 
 
-running = True
+def repeat(running):
+    while running:
+        print('\n'
+   """
+      '''''''''''''''''''''''''''''''''''''''
+      '         INPUT:                      '
+      '   V  - to video without sound       '
+      '   M  - music                        '
+      '   VM - video with music             '                   
+      '                                     '
+      '''''''''''''''''''''''''''''''''''''''
+      """)
+        print("\n")
+        global choice_filter 
+        choice_filter= input("Input: ")
+        if choice_filter == "M":
+            stream = yt.streams.filter(only_audio = True, file_extension='mp4')
+            running = False
+        elif choice_filter == "V":
+            stream = yt.streams.filter(only_video = True, file_extension='mp4')
+            running = False
+        elif choice_filter == "VM":
+            stream = yt.streams.filter(progressive = True, file_extension='mp4')
+            running = False
 
-while running:
-    choice_filter = input("Input V or M to filter. V - video; M - music: ")
-    if choice_filter == "M":
-        stream = yt.streams.filter(only_audio = True, file_extension='mp4')
-        running = False
+    
+    return stream
+    # Don't wanna get choice_filter =/
 
-    if choice_filter == "V":
-        stream = yt.streams.filter(only_video = True, file_extension='mp4')
-        running = False
 
+def search_string():
+    stream = repeat(running)
     reverse_list = ""
     for i in (stream):
         reverse_list += str(i)
 
-    pattern_search = (r'(res="[+]?\d+p)')
+    if choice_filter == "V":
+        pattern_search = (r'(res="[+]?\d+p)')
+    elif choice_filter == "M":
+        pattern_search = (r'(abr="[1-9]{3}kbps)"')
+    elif choice_filter == "VM":
+        pattern_search = (r'(res="[+]?\d+p)')
+        
     pattern_result = re.findall(pattern_search, reverse_list)
+
     
     #pattern
-    template_video = " type=video"
     reverse_str = str(pattern_result)
     clean_str = reverse_str.replace("'",'').replace('"', '').replace('(', '').replace(')', '').replace(',','').replace('[', '').replace("]",'')
     spl_string = clean_str.split()
 
 
     # Add pattern to value after every iteration
-    result = list(s + template_video for s in spl_string)
-    
-    video_view = result.copy()
+    if choice_filter == "M":
+        result = list(s + template_music for s in spl_string)
+    else:
+        result = list(s + template_video for s in spl_string)
+
+    view_template = result.copy()
 
     count = 1
-    for x in video_view:
+    for x in view_template:
         print(f"{count}. {x}")
         count += 1
 
     #pattern
-    type_pattern_get = "mp4"
-    input_resolution = input("Type video resolution. For example: 1080p or 144p: ")
-    filter_to_download = stream.filter(subtype="mp4", resolution=input_resolution).first()
+    if choice_filter == "M":
+        input_resolution = input("Choice abr bitrate like: 128kbps: ")
+        filter_to_download = stream.filter(subtype="mp4", abr=input_resolution).first()
+    else:
+        input_resolution = input("Type video resolution. For example: 1080p or 144p: ")
+        filter_to_download = stream.filter(subtype="mp4", resolution=input_resolution).first()
+
+
+
     filter_to_download.download()
 
         
 
+search_string()
 
 
-
-
-
-#config_atrr = ('scheme', 'netloc')
-#test = urlparse("https://www.youtube.com/")
-
-
-#def request_download():
-    #get_url = input("Paste there link of video what you wand download: ")
-
-
-   # return get_url
-
-
-#def asker():
-    #get_answer = input("Video or Music? V - video; M - music ")
-    #return get_answer
-    
-
-
-##request_download()
-##asker()
